180f4dac759814aa89882dbdeb414335d09ada41
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-10-06 14:03:57 -0400

    feat: store tasks in db

--- db/tasks.go
+++ db/tasks.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -9,11 +10,13 @@ import (
 var taskBucket = []byte("tasks")
 var db *bolt.DB
 
+// Task as an object
 type Task struct {
 	Key   int
 	Value string
 }
 
+// Init starts the database in the main function
 func Init(dbPath string) error {
 	var err error
 	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
@@ -24,4 +27,30 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+// CreateTask adds a task object to the database
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func itob(v int) []byte {
+	bs := make([]byte, 8)
+	binary.BigEndian.PutUint64(bs, uint64(v))
+	return bs
+}
+
+func btoi(bs []byte) int {
+	return int(binary.BigEndian.Uint64(bs))
 }
